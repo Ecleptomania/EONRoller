6a126290256396980ad31355ad5d06a04148f3fa
Niklas Axelsson <niqlas.axelsson@gmail.com>
2020-11-28 15:58:56 +0100

    Got a response working
    Lots of changes in the code, got a response working.

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+/*
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
@@ -9,13 +10,15 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   //token: auth.token,
+   token : 'NzgyMjAyNzI4ODYzNjI5MzIz.X8IxDw.bLR1MmEcIPCIYABxl-a98oJF4Dw',
    autorun: true
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    console.log('bot is ready');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
@@ -37,3 +40,93 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
+*/
+
+
+const Discord = require("discord.js");
+const prefix = '!'
+
+const bot = new Discord.Client();
+const token = 'NzgyMjAyNzI4ODYzNjI5MzIz.X8IxDw.bLR1MmEcIPCIYABxl-a98oJF4Dw';
+
+
+
+bot.on('ready', () => {
+  console.log('bot is ready')
+})
+
+bot.login(token)
+
+
+
+bot.on('message', async (msg) => {
+  //if our message doesnt start with our defined prefix, dont go any further into function
+  if(!msg.content.startsWith(prefix)) {
+    console.log('no prefix')
+    return
+  }
+  
+  //slices off prefix from our message, then trims extra whitespace, then returns our array of words from the message
+  const args = msg.content.slice(prefix.length).trim().split(' ')
+  
+  //splits off the first word from the array, which will be our command
+  const command = args.shift().toLowerCase()
+  //log the command
+  console.log('command: ', command)
+  //log any arguments passed with a command
+  console.log(args)
+
+  if(command === 'ego') {
+    msg.react("😀")
+    msg.reply('wow, what a great post')
+  }
+
+  if (command === "clear") {
+    //default deletes message itself plus previous
+    let num = 2;
+    
+    //if argument is provided, we need to convert it from string to number
+    if (args[0]) {
+      //add 1 to delete clear command itself
+      num = parseInt(args[0]) + 1;
+    }
+    //bulk delete the messages
+    msg.channel.bulkDelete(num);
+    //notify channel of deleted messages
+    msg.channel.send(`deleted  ${args[0]} posts for you`);
+  }
+  
+})
+
+/*
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+
+bot.on('message', function (user, userID, channelID, message1, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+
+
+    if (message1.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+*/
